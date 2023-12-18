b28e2fddb91db72f5ee1bfcfd35718a0fe88598d
Muhammed Ajmal <ajuajmal1722001@gmail.com>
2023-12-18 09:29:15 +0530

    3

--- index.js
+++ index.js
@@ -48,4 +48,23 @@ function isIsogram(str) {
     return true;
   }
  
+
+  // If we list all the natural numbers below 10 that are multiples of 3 or 5, we get 3, 5, 6 and 9. The sum of these multiples is 23.
+
+  // Finish the solution so that it returns the sum of all the multiples of 3 or 5 below the number passed in.
   
+  // Additionally, if the number is negative, return 0.
+  
+  // Note: If the number is a multiple of both 3 and 5, only count it once.
+  
+  function solution(number){
+    let sum = 0;
+    for (let i = 0; i < number; i++) {
+      if (i % 3 === 0 || i % 5 === 0) {
+        sum += i;
+      }
+    }
+    console.log(sum)
+    return sum;
+  }
+  solution(10)
