2d18a3421e75bfb3fe1108f3fec8b5a1f067ec91
Muhammed Ajmal <ajuajmal1722001@gmail.com>
2023-12-18 09:45:21 +0530

    4

--- index.js
+++ index.js
@@ -67,4 +67,23 @@ function isIsogram(str) {
     console.log(sum)
     return sum;
   }
-  solution(10)
+
+
+//  Write a function that accepts an array of 10 integers (between 0 and 9), that returns a string of those numbers in the form of a phone number.
+
+// Example
+// createPhoneNumber([1, 2, 3, 4, 5, 6, 7, 8, 9, 0]) // => returns "(123) 456-7890"
+// The returned format must be correct in order to complete this challenge.
+
+// Don't forget the space after the closing parentheses!
+
+function createPhoneNumber(numbers){
+  var format = "(xxx) xxx-xxxx";
+  
+  for(var i = 0; i < numbers.length; i++)
+  {
+    format = format.replace('x', numbers[i]);
+  }
+  
+  return format;
+}
