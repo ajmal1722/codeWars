7972c1f48f0bb2a05d87cf9817c6cccd7a6fa364
Muhammed Ajmal <ajuajmal1722001@gmail.com>
2023-12-21 09:30:15 +0530

    9

--- index.js
+++ index.js
@@ -184,3 +184,22 @@ function order(words) {
   // Join the sorted words into a string and return
   return sortedWords.join(" ");
 }
+
+
+
+// Return the number (count) of vowels in the given string.
+
+// We will consider a, e, i, o, u as vowels for this Kata (but not y).
+
+// The input string will only consist of lower case letters and/or spaces.
+
+function getCount(str) {
+  const vowels = 'aeiou'
+  let count = 0;
+  const vowelCount = str.split('').filter(x => {
+    if (vowels.includes(x)){
+      count ++;
+    }
+  })
+  return count
+}
