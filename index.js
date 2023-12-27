8ad4ef94e30e1a645e25caa6a6d2e01e37ed94f6
Muhammed Ajmal <ajuajmal1722001@gmail.com>
2023-12-27 10:03:03 +0530

    10

--- index.js
+++ index.js
@@ -203,3 +203,47 @@ function getCount(str) {
   })
   return count
 }
+
+
+
+
+// Complete the method/function so that it converts dash/underscore 
+// delimited words into camel casing. The first word within the 
+// output should be capitalized only if the original word was 
+// capitalized (known as Upper Camel Case, also often referred to as 
+// Pascal case). The next words should be always capitalized.
+
+// Examples
+// "the-stealth-warrior" gets converted to "theStealthWarrior"
+
+// "The_Stealth_Warrior" gets converted to "TheStealthWarrior"
+
+// "The_Stealth-Warrior" gets converted to "TheStealthWarrior"
+
+
+
+
+
+// A Narcissistic Number (or Armstrong Number) is a positive number which is the sum of its own digits, each raised to the power of the number of digits in a given base. In this Kata, we will restrict ourselves to decimal (base 10).
+
+// For example, take 153 (3 digits), which is narcissistic:
+
+//     1^3 + 5^3 + 3^3 = 1 + 125 + 27 = 153
+// and 1652 (4 digits), which isn't:
+
+//     1^4 + 6^4 + 5^4 + 2^4 = 1 + 1296 + 625 + 16 = 1938
+// The Challenge:
+
+// Your code must return true or false (not 'true' and 'false') depending upon whether the given number is a Narcissistic number in base 10.
+
+// This may be True and False in your language, e.g. PHP.
+
+// Error checking for text strings or other invalid inputs is not required, only valid positive non-zero integers will be passed into the function.
+
+function narcissistic(value) {
+  const val = value.toString().split('');
+  const result = val.reduce((acc, cur) => acc + (cur ** val.length),0);
+  console.log(typeof(result))
+  console.log((result))
+  return parseInt(result) === value;
+}
