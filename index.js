239f81b5d0ec1632db0c285fc875676bb55f02fd
Muhammed Ajmal <ajuajmal1722001@gmail.com>
2024-01-02 09:45:25 +0530

    13

--- index.js
+++ index.js
@@ -314,3 +314,18 @@ function likes(names) {
     return `${names[0]} ${names[1]} and ${num} others like this`
   }
 }
+
+
+
+// Your goal in this kata is to implement a difference function, which subtracts one list from another and returns the result.
+
+// It should remove all values from list a, which are present in list b keeping their order.
+
+// arrayDiff([1,2],[1]) == [2]
+// If a value is present in b, all of its occurrences must be removed from the other:
+
+// arrayDiff([1,2,2,2,3],[2]) == [1,3]
+
+function arrayDiff(a, b) {
+  return   a.filter(item => !b.includes(item)) 
+}
