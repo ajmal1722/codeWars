722841a3e2aaefbad6d38c52b0f618d9dfeb0228
Muhammed Ajmal <ajuajmal1722001@gmail.com>
2024-01-05 09:43:10 +0530

    16

--- index.js
+++ index.js
@@ -363,3 +363,32 @@ function findUniq(arr) {
   return parseFloat(Object.keys(counts).find(key => counts[key] === 1));
 }
 findUniq([ 1, 0, 0 ])
+
+
+
+
+// Write a function that takes in a string of one or more words, 
+// and returns the same string, but with all words that have five or 
+// more letters reversed (Just like the name of this Kata). 
+// Strings passed in will consist of only letters and spaces. 
+// Spaces will be included only when more than one word is present.
+
+// Examples:
+
+// "Hey fellow warriors"  --> "Hey wollef sroirraw" 
+// "This is a test        --> "This is a test" 
+// "This is another test" --> "This is rehtona test"
+
+function spinWords(string){
+  const words = string.split(' ')
+  
+  const result = words.map(item => {
+    if (item.length >= 5){
+      item = item.split('').reverse().join('')
+      return item
+    } else {
+      return item
+    }
+  })
+  return result.join(' ')
+}
