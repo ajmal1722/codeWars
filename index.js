7c0548ef9db96ce2b287237c06bcbbebed079000
Muhammed Ajmal <ajuajmal1722001@gmail.com>
2024-01-06 10:30:22 +0530

    18

--- index.js
+++ index.js
@@ -356,7 +356,7 @@ findShort('Testing for fixed tests');
 function findUniq(arr) {
   const counts = arr.reduce((acc, num) => {
     acc[num] = (acc[num] || 0) + 1;
-    console.log(acc[num])
+    // console.log(acc[num])
     return acc;
   }, {});
 
@@ -407,3 +407,74 @@ function spinWords(string){
 function solution(str, ending){
   return str.endsWith(ending)
 }
+
+
+
+
+// Some numbers have funny properties. For example:
+
+// 89 --> 8¹ + 9² = 89 * 1
+// 695 --> 6² + 9³ + 5⁴= 1390 = 695 * 2
+// 46288 --> 4³ + 6⁴+ 2⁵ + 8⁶ + 8⁷ = 2360688 = 46288 * 51
+// Given two positive integers n and p, we want to find a positive integer k, if it exists, such that the sum of the digits of n raised to consecutive powers starting from p is equal to k * n.
+
+// In other words, writing the consecutive digits of n as a, b, c, d ..., is there an integer k such that :
+
+// (
+// �
+// �
+// +
+// �
+// �
+// +
+// 1
+// +
+// �
+// �
+// +
+// 2
+// +
+// �
+// �
+// +
+// 3
+// +
+// .
+// .
+// .
+// )
+// =
+// �
+// ∗
+// �
+// (a 
+// p
+//  +b 
+// p+1
+//  +c 
+// p+2
+//  +d 
+// p+3
+//  +...)=n∗k
+// If it is the case we will return k, if not return -1.
+
+// Note: n and p will always be strictly positive integers.
+
+// Examples:
+// n = 89; p = 1 ---> 1 since 8¹ + 9² = 89 = 89 * 1
+
+// n = 92; p = 1 ---> -1 since there is no k such that 9¹ + 2² equals 92 * k
+
+// n = 695; p = 2 ---> 2 since 6² + 9³ + 5⁴= 1390 = 695 * 2
+
+// n = 46288; p = 3 ---> 51 since 4³ + 6⁴+ 2⁵ + 8⁶ + 8⁷ = 2360688 = 46288 * 51
+
+function digPow(n, p){
+  const sum = n.toString().split('').reduce((acc, val) => acc + Math.pow(val, +p++), 0 )
+  let k = 1;
+  while(n * k !== sum && n * k < sum) { 
+    k++;
+  }
+  return n * k === sum ? k : -1
+}
+digPow(89, 1)
