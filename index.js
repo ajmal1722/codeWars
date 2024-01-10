74e45aa021c630bae009bd3406e88c394b756bf1
Muhammed Ajmal <ajuajmal1722001@gmail.com>
2024-01-10 11:08:33 +0530

    22

--- index.js
+++ index.js
@@ -561,3 +561,35 @@ function count(string) {
    return occurrence
  }
  count('abac');
+
+
+
+//  Given two integers a and b, which can be positive or negative, find 
+//  the sum of all the integers between and including them and 
+//  return it. If the two numbers are equal return a or b.
+
+// Note: a and b are not ordered!
+
+// Examples (a, b) --> output (explanation)
+// (1, 0) --> 1 (1 + 0 = 1)
+// (1, 2) --> 3 (1 + 2 = 3)
+// (0, 1) --> 1 (0 + 1 = 1)
+// (1, 1) --> 1 (1 since both are same)
+// (-1, 0) --> -1 (-1 + 0 = -1)
+// (-1, 2) --> 2 (-1 + 0 + 1 + 2 = 2)
+
+function getSum(a, b){
+  let sum = 0;
+  
+  if (a > b){
+    [b, a] = [a, b] // swapping the values here
+  }
+  
+  for (let i = a; i <= b; i++){
+    sum += i
+    console.log(i);
+  }
+  console.log(sum)
+ return sum
+}
+getSum(0,0)
