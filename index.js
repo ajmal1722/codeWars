ec9d7993552e01cc63dad116d0b3d94f76391796
Muhammed Ajmal <ajuajmal1722001@gmail.com>
2024-01-10 09:40:22 +0530

    21

--- index.js
+++ index.js
@@ -542,3 +542,22 @@ function duplicateCount(text){
   return count
 }
 duplicateCount("abcdea")
+
+
+
+// The main idea is to count all the occurring characters in a string. 
+// If you have a string like aba, then the result should be {'a': 2, 'b': 1}.
+
+// What if the string is empty? Then the result should be empty object literal, {}.
+
+function count(string) {
+  let occurrence = {};
+    string.split('').forEach(x => {
+     if (x.match(/[a-zA-z]/)){
+        occurrence[x] = (occurrence[x] || 0) + 1;
+        // console.log(occurrence)
+     }
+   })
+   return occurrence
+ }
+ count('abac');
