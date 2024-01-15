402daed0e06725f95a4ecdfd426401b4207b3857
Muhammed Ajmal <ajuajmal1722001@gmail.com>
2024-01-15 09:36:18 +0530

    26

--- index.js
+++ index.js
@@ -672,3 +672,21 @@ function findOdd(A) {
   }
 }
 findOdd([1,1,2,-2,5,2,4,4,-1,-2,5])
+
+
+// 26.
+// This time no story, no theory. The examples below show you how to write function accum:
+
+// Examples:
+// accum("abcd") -> "A-Bb-Ccc-Dddd"
+// accum("RqaEzty") -> "R-Qq-Aaa-Eeee-Zzzzz-Tttttt-Yyyyyyy"
+// accum("cwAt") -> "C-Ww-Aaa-Tttt"
+// The parameter of accum is a string which includes only letters from a..z and A..Z.
+function accum(s) {
+	return s.split('').map((char, index) => {
+    const repeatedArray = char.repeat(index + 1)
+    console.log(`${repeatedArray.charAt(0).toUpperCase()}${repeatedArray.slice(1).toLowerCase()}`)
+    return `${repeatedArray.charAt(0).toUpperCase()}${repeatedArray.slice(1).toLowerCase()}`
+  } ). join('-')
+}
+accum("ZpglnRxqenU")
