5195e5afdfafb365bcd6e7275b8992fed1040ba9
Muhammed Ajmal <ajuajmal1722001@gmail.com>
2024-01-17 12:26:29 +0530

    28

--- index.js
+++ index.js
@@ -704,3 +704,22 @@ function solution(string) {
   return string.replace(/([a-z])([A-Z])/g, '$1 $2')
 }
 solution('camelCasingTest')
+
+
+// 28.
+// Welcome. In this kata, you are asked to square every digit of a number and concatenate them.
+
+// For example, if we run 9119 through the function, 811181 will come out, because 92 is 81 and 12 is 1. (81-1-1-81)
+
+// Example #2: An input of 765 will/should return 493625 because 72 is 49, 62 is 36, and 52 is 25. (49-36-25)
+
+// Note: The function accepts an integer and returns an integer.
+function squareDigits(num){
+  const numbers = num.toString().split('')
+  
+  const sumOfNums = numbers.reduce((sum, val) => sum + String(val * val), 0)
+  console.log('sum:',sumOfNums)
+  console.log(sumOfNums.slice(1));
+  return sumOfNums.slice(1) * 1
+}
+squareDigits(32132)
