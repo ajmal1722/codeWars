f7fff6cc6d896da151eb7dd6511768c04e3d950c
Muhammed Ajmal <ajuajmal1722001@gmail.com>
2024-01-18 09:59:29 +0530

    29

--- index.js
+++ index.js
@@ -718,8 +718,31 @@ function squareDigits(num){
   const numbers = num.toString().split('')
   
   const sumOfNums = numbers.reduce((sum, val) => sum + String(val * val), 0)
-  console.log('sum:',sumOfNums)
-  console.log(sumOfNums.slice(1));
+  // console.log('sum:',sumOfNums)
+  // console.log(sumOfNums.slice(1));
   return sumOfNums.slice(1) * 1
 }
 squareDigits(32132)
+
+// 29. 
+// Given an integer as input, can you round it to the next (meaning, "greater than or equal") multiple of 5?
+
+// Examples:
+
+// input:    output:
+// 0    ->   0
+// 2    ->   5
+// 3    ->   5
+// 12   ->   15
+// 21   ->   25
+// 30   ->   30
+// -2   ->   0
+// -5   ->   -5
+// etc.
+// Input may be any positive or negative integer (including 0).
+
+// You can assume that all inputs are valid integers.
+
+function roundToNext5(n){
+  return  Math.ceil(n / 5) * 5
+}
