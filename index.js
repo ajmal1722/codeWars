504a2f926b2b6140ce18d3744be87fd47db99a15
Muhammed Ajmal <ajuajmal1722001@gmail.com>
2024-01-25 09:58:06 +0530

    35

--- index.js
+++ index.js
@@ -880,3 +880,20 @@ function reverseWords(str) {
   return words.map(x => x.split('').reverse().join('')).join(' ')
 }
 // console.log(reverseWords('this is an example'))
+
+
+
+
+// 35.
+// Given a list of integers, determine whether the sum of its elements is odd or even.
+
+// Give your answer as a string matching "odd" or "even".
+
+// If the input array is empty consider it as: [0] (array with a zero).
+
+function oddOrEven(array) {
+  //enter code here
+ const sum = array.reduce((acc,val) => acc + val, 0)
+ 
+ return sum % 2 === 0 ? 'even' : 'odd'
+}
