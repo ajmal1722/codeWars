fa127bcbce4ecc0b47be249f749987be4eb2c477
Muhammed Ajmal <ajuajmal1722001@gmail.com>
2024-01-31 09:39:07 +0530

    39

--- index.js
+++ index.js
@@ -991,3 +991,22 @@ const binaryArrayToNumber = arr => {
   return parseInt(arr.join(''), 2); // 
 };
 binaryArrayToNumber([0,0,1,0])
+
+
+
+// 39.
+// Implement a function that adds two numbers together and returns their sum in binary. 
+// The conversion can be done before, or after the addition.
+
+// The binary number returned should be a string.
+
+// Examples:(Input1, Input2 --> Output (explanation)))
+
+// 1, 1 --> "10" (1 + 1 = 2 in decimal or 10 in binary)
+// 5, 9 --> "1110" (5 + 9 = 14 in decimal or 1110 in binary)
+
+function addBinary(a,b) {
+  return (a + b).toString(2)
+  // The 2 parameter in toString specifies the base, which is 2 for binary.
+}
+console.log(addBinary(1,2))
