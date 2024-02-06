96322c9ad6ec31959c5b5f44dd67ce0e6a27e209
Muhammed Ajmal <ajuajmal1722001@gmail.com>
2024-02-06 09:55:07 +0530

    43

--- index.js
+++ index.js
@@ -1072,3 +1072,30 @@ function filter_list(l) {
 String.prototype.isUpperCase = function() {
   return this.toString() === this.toUpperCase();
 }
+
+
+
+// 43.
+// As a part of this Kata, you need to create a function that when provided with a triplet, 
+// returns the index of the numerical element that lies between the other two elements.
+
+// The input to the function will be an array of three distinct numbers (Haskell: a tuple).
+
+// For example:
+
+// gimme([2, 3, 1]) => 0
+// 2 is the number that fits between 1 and 3 and the index of 2 in the input array is 0.
+
+// Another example (just to make sure it is clear):
+
+// gimme([5, 10, 14]) => 1
+// 10 is the number that fits between 5 and 14 and the index of 10 in the input array is 1
+
+function gimme (triplet) {
+  const s = triplet.slice(0,3)
+  triplet.sort((a, b) => a - b);
+  console.log('s =', s)
+  console.log('trip =', triplet)
+  return s.findIndex(elm => elm === triplet[1]) 
+}
+// console.log(gimme([2, 3, 1]));
