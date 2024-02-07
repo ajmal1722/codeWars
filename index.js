ce647484e72ea4ef07e3c92f6703ce885574bafa
Muhammed Ajmal <ajuajmal1722001@gmail.com>
2024-02-07 09:22:21 +0530

    44

--- index.js
+++ index.js
@@ -1099,3 +1099,24 @@ function gimme (triplet) {
   return s.findIndex(elm => elm === triplet[1]) 
 }
 // console.log(gimme([2, 3, 1]));
+
+
+// 44.
+// Complete the findNextSquare method that finds the next integral perfect square after the 
+// one passed as a parameter. Recall that an integral perfect square is an integer n 
+// such that sqrt(n) is also an integer.
+
+// If the parameter is itself not a perfect square then -1 should be returned. 
+// You may assume the parameter is non-negative.
+
+// Examples:(Input --> Output)
+
+// 121 --> 144
+// 625 --> 676
+// 114 --> -1 since 114 is not a perfect square
+
+function findNextSquare(sq) {
+  // Return the next square if sq is a perfect square, -1 otherwise
+  const sqrt = Math.sqrt(sq);
+  return sqrt % 1 === 0 ? (sqrt + 1) * (sqrt + 1) : -1;
+}
