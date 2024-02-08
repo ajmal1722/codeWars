3a995312e6e4583421ef67a8a9940cb0e0402d86
Muhammed Ajmal <ajuajmal1722001@gmail.com>
2024-02-08 09:23:23 +0530

    45

--- index.js
+++ index.js
@@ -1120,3 +1120,22 @@ function findNextSquare(sq) {
   const sqrt = Math.sqrt(sq);
   return sqrt % 1 === 0 ? (sqrt + 1) * (sqrt + 1) : -1;
 }
+
+
+// 45.
+// Given a string str, reverse it and omit all non-alphabetic characters.
+
+// Example
+// For str = "krishan", the output should be "nahsirk".
+
+// For str = "ultr53o?n", the output should be "nortlu".
+
+// Input/Output
+// [input] string str
+// A string consists of lowercase latin letters, digits and symbols.
+
+// [output] a string
+
+function reverseLetter(str) {
+  return str.split('').filter(x => /[a-z]/.test(x)).reverse().join('')
+}
