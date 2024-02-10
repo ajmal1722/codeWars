75bd36af6bb8d13b7a69ebf19b31e9e39cc1fe91
Muhammed Ajmal <ajuajmal1722001@gmail.com>
2024-02-10 10:36:36 +0530

    46

--- index.js
+++ index.js
@@ -1139,3 +1139,15 @@ function findNextSquare(sq) {
 function reverseLetter(str) {
   return str.split('').filter(x => /[a-z]/.test(x)).reverse().join('')
 }
+
+
+
+// // 46.
+// Write a function to convert a name into initials. This kata strictly
+//  takes two words with one space in between them.
+
+// The output should be two capital letters with a dot separating them.
+function abbrevName(name){
+  const names = name.split(' ')
+  return names.map(x => x.charAt(0).toUpperCase()).join('.')
+}
