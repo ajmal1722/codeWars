e04cf2452aac950c9be762e919581179fa598996
Muhammed Ajmal <ajuajmal1722001@gmail.com>
2024-02-13 09:47:59 +0530

    48

--- index.js
+++ index.js
@@ -1174,4 +1174,34 @@ function factorial(n)
   }
   return sum
 }
-console.log(factorial (3))
+// console.log(factorial (3));
+
+
+
+// 48.
+// Implement a function that accepts 3 integer values a, b, c. The function should 
+// return true if a triangle can be built with the sides of given length and false 
+// in any other case.
+
+// (In this case, all triangles must have surface greater than 0 to be accepted).
+
+// Examples:
+
+// Input -> Output
+// 1,2,2 -> true
+// 4,2,3 -> true
+// 2,2,2 -> true
+// 1,2,3 -> false
+// -5,1,3 -> false
+// 0,2,3 -> false
+// 1,2,9 -> false 
+
+function isTriangle(a, b, c) {
+  if (a <= 0 || b <= 0 || c <= 0) {
+    return false; // All sides must be greater than 0
+  }
+
+  // Check if the sum of any two sides is greater than the third side
+  return a + b > c && a + c > b && b + c > a;
+}
+console.log(isTriangle(1, 2, 2));
