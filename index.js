88372f7aa5c573a1c080da0e7c7288ecf1da7f06
Muhammed Ajmal <ajuajmal1722001@gmail.com>
2024-02-14 09:28:58 +0530

    49

--- index.js
+++ index.js
@@ -1205,3 +1205,17 @@ function isTriangle(a, b, c) {
   return a + b > c && a + c > b && b + c > a;
 }
 console.log(isTriangle(1, 2, 2));
+
+
+// // 49.
+// Create a function that returns the sum of the two lowest positive numbers given 
+// an array of minimum 4 positive integers. No floats or non-positive integers will be passed.
+
+// For example, when an array is passed like [19, 5, 42, 2, 77], the output should be 7.
+
+// [10, 343445353, 3453445, 3453545353453] should return 3453455.
+
+function sumTwoSmallestNumbers(numbers) {  
+  numbers.sort((a, b) => a - b)
+  return numbers[0] + numbers[1]
+}
