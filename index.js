bf0dfa317e38377def1bc0a91ad3d44a99086bcd
Muhammed Ajmal <ajuajmal1722001@gmail.com>
2024-02-15 10:03:38 +0530

    50

--- index.js
+++ index.js
@@ -1219,3 +1219,40 @@ function sumTwoSmallestNumbers(numbers) {
   numbers.sort((a, b) => a - b)
   return numbers[0] + numbers[1]
 }
+
+
+// 50.
+// The first input array is the key to the correct answers to an exam, 
+// like ["a", "a", "b", "d"]. The second one contains a student's submitted answers.
+
+// The two arrays are not empty and are the same length. Return the score 
+// for this array of answers, giving +4 for each correct answer, -1 for each 
+// incorrect answer, and +0 for each blank answer, represented as an empty 
+// string (in C the space character is used).
+
+// If the score < 0, return 0.
+
+// For example:
+
+// checkExam(["a", "a", "b", "b"], ["a", "c", "b", "d"]) → 6
+// checkExam(["a", "a", "c", "b"], ["a", "a", "b",  ""]) → 7
+// checkExam(["a", "a", "b", "c"], ["a", "a", "b", "c"]) → 16
+// checkExam(["b", "c", "b", "a"], ["",  "a", "a", "c"]) → 0
+
+function checkExam(array1, array2) {
+  let sum = 0;
+ for (let i = 0; i < array1.length; i++) {
+   if (array2[i] === ""){
+     sum += 0;
+     console.log(sum, '0')
+   } else if (array1[i] === array2[i]){
+     sum += 4
+     console.log(sum, '+4')
+   } else {
+     sum = sum - 1
+     console.log(sum, '-1')
+   }
+ }
+  return sum < 0 ? 0: sum;
+}
+// console.log(checkExam(["b", "c", "b", "a"], ["",  "a", "a", "c"]))
