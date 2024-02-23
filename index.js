6f7e6e97460372c5b8d06d5c1cce31e97cddc10c
Muhammed Ajmal <ajuajmal1722001@gmail.com>
2024-02-23 09:41:42 +0530

    55

--- index.js
+++ index.js
@@ -1352,3 +1352,23 @@ oddOnesOut([1, 2, 3, 1, 3, 3]);
 sortme = function( names ){
   return names.sort((a, b) => a.toLowerCase().localeCompare(b.toLowerCase()));
 }
+
+
+// 55.
+// Write a function that takes a positive integer n, sums all the cubed values 
+// from 1 to n (inclusive), and returns that sum.
+
+// Assume that the input n will always be a positive integer.
+
+// Examples: (Input --> output)
+
+// 2 --> 9 (sum of the cubes of 1 and 2 is 1 + 8)
+// 3 --> 36 (sum of the cubes of 1, 2, and 3 is 1 + 8 + 27)
+
+function sumCubes(n){
+  let arr = []
+ for (let i = 1; i <= n; i++) {
+   arr.push(i)
+ } 
+  return arr.reduce((sum, val) => sum + val**3, 0)
+}
