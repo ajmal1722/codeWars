12c12f5aefd2aeb7bdb01ca56e02453f7da1f05c
Muhammed Ajmal <ajuajmal1722001@gmail.com>
2024-02-27 10:19:20 +0530

    58

--- index.js
+++ index.js
@@ -1442,3 +1442,23 @@ function deleteNth(arr,n){
   return res;
 }
 deleteNth([20,37,20,21], 1);
+
+
+
+// 58.
+// Take 2 strings s1 and s2 including only letters from a to z. Return a new sorted 
+// string, the longest possible, containing distinct letters - each taken only 
+// once - coming from s1 or s2.
+
+// Examples:
+// a = "xyaabbbccccdefww"
+// b = "xxxxyyyyabklmopq"
+// longest(a, b) -> "abcdefklmopqwxy"
+
+// a = "abcdefghijklmnopqrstuvwxyz"
+// longest(a, a) -> "abcdefghijklmnopqrstuvwxyz"
+
+function longest(s1, s2) {
+  return [...new Set([...s1, ...s2])].sort().join('');
+}
+longest("aretheyhere", "yestheyarehere")
