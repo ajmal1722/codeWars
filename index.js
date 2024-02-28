995a89705dc1ae1b7ee65dfc598c2aca50efcff9
Muhammed Ajmal <ajuajmal1722001@gmail.com>
2024-02-28 10:00:35 +0530

    59

--- index.js
+++ index.js
@@ -1462,3 +1462,18 @@ function longest(s1, s2) {
   return [...new Set([...s1, ...s2])].sort().join('');
 }
 longest("aretheyhere", "yestheyarehere")
+
+
+// 59.
+// A palindrome is a series of characters that read the same forwards 
+// as backwards such as "hannah", "racecar" and "lol".
+
+// For this Kata you need to write a function that takes a string of 
+// characters and returns the length, as an integer value, of longest 
+// alphanumeric palindrome that could be made by combining the characters 
+// in any order but using each character only once. The function 
+// should not be case sensitive.
+
+// For example if passed "Hannah" it should return 6 and if passed 
+// "aabbcc_yYx_" it should return 9 because one possible palindrome 
+// would be "abcyxycba".
