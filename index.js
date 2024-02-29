fe27472eaf72c3b2f6b30566e15fec1818893957
Muhammed Ajmal <ajuajmal1722001@gmail.com>
2024-02-29 10:21:26 +0530

    60

--- index.js
+++ index.js
@@ -1477,3 +1477,30 @@ longest("aretheyhere", "yestheyarehere")
 // For example if passed "Hannah" it should return 6 and if passed 
 // "aabbcc_yYx_" it should return 9 because one possible palindrome 
 // would be "abcyxycba".
+
+
+
+// 60.
+// In this kata you get the start number and the end number of a region and 
+// should return the count of all numbers except numbers with a 5 in it. 
+// The start and the end number are both inclusive!
+
+// Examples:
+
+// 1,9 -> 1,2,3,4,6,7,8,9 -> Result 8
+// 4,17 -> 4,6,7,8,9,10,11,12,13,14,16,17 -> Result 12
+// The result may contain fives. ;-)
+// The start number will always be smaller than the end number. 
+// Both numbers can be also negative!
+
+function dontGiveMeFive(start, end) {
+  let count = 0;
+
+  for (let i = start; i <= end; i++) {
+    if (!String(i).includes('5')) {
+      count++;
+    }
+  }
+
+  return count;
+}
