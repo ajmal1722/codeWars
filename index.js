082e4267e411ade917514b916d35f57d1bba7927
Muhammed Ajmal <ajuajmal1722001@gmail.com>
2024-03-02 09:34:41 +0530

    62

--- index.js
+++ index.js
@@ -1536,3 +1536,14 @@ function isPrime(num) {
   }
   return check === 1 ? false : true
 }
+
+
+// 62. Write a function that returns a string in which firstname is swapped with last name.
+
+// Example(Input --> Output)
+
+// "john McClane" --> "McClane john"
+
+function nameSuffle(str){
+  return str.split(' ').reverse().join(' ')
+}
